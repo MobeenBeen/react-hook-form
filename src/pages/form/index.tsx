98f39d8bd74c1bbc71dcf28589b85306496a7f7a
MobeenBeen <mobeenislam7861@gmail.com>
2025-07-28 23:34:29 +0500

    dynamic array

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { userSchema, UserFormSchema } from "@/user.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -9,12 +9,19 @@ export default function Form() {
     watch,
     reset,
     getFieldState,
+    control,
     formState: { errors },
   } = useForm<UserFormSchema>({
     resolver: zodResolver(userSchema),
-    defaultValues: { name: "noName" },
+    defaultValues: { name: "noName",
+        users:[{education:'nil', certification:'nil'}]
+     },
   });
 
+  const { fields, append } = useFieldArray({
+    control,
+    name: "users",
+  });
   const onSubmit = (data: UserFormSchema) => {
     console.log("FormData", data);
   };
@@ -23,6 +30,18 @@ export default function Form() {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
+
+        <h2>dynamic form</h2>
+        {fields.map((field,index)=>(
+            <div key={field.id}>
+            <label>education</label>
+            <input {...register(`users.${index}.education`)}/>            
+            <label>certification</label>
+            <input {...register(`users.${index}.certification`)}/>          
+
+            </div>
+        ))}
+        <button onClick={()=> append({education:'',certification:''})}>Add more</button>
         <div>
           <label>name</label>
           <input type="text" {...register("name")} />{" "}
--- src/user.schema.ts
+++ src/user.schema.ts
@@ -4,7 +4,11 @@ export const userSchema = z.object({
     name: z.string().optional(),
     email: z.email().optional(),
     age:z.number().optional(),
-    gender:z.string().optional()
+    gender:z.string().optional(),
+    users:z.array(z.object({
+        education:z.string().optional(),
+        certification:z.string().optional()
+    }))
 })
 
 
